8a308fa35f712d16423fff56aa537e117d962290
sizuchok <sizuchok0@gmail.com>
2024-01-21 14:56:49 +0200

    feat: Implement hashing service
    Add an abstract hashing class and its implementation using crypto (CryptoService)

---
+++ src/services/hashing/crypto.service.ts
@@ -0,0 +1,24 @@
+import { pbkdf2, randomBytes } from 'crypto'
+import { HashingService } from './hashing.service'
+
+export class CryptoService implements HashingService {
+  async hash(data: string, salt?: string) {
+    salt ??= randomBytes(32).toString('hex')
+    return new Promise<string>((resolve, reject) => {
+      pbkdf2(data, salt!, 10000, 64, 'sha512', (err, derivedKey) => {
+        if (err) {
+          return reject(err)
+        }
+        const hash = derivedKey.toString('hex')
+        const hashAndSalt = `${hash}.${salt}`
+        resolve(hashAndSalt)
+      })
+    })
+  }
+
+  async compare(data: string, encrypted: string) {
+    const salt = encrypted.split('.')[1]
+    const hash = await this.hash(data, salt)
+    return hash === encrypted
+  }
+}
